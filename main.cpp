f4dc690da362bc3a03d5f8ff6d2856e10d510a95
kzielin4 <kamilpiotrlukaszzielinski@gmail.com>
2018-01-21 15:29:14 +0100

    Change all files - copy PORR version

--- main.cpp
+++ main.cpp
@@ -3,17 +3,18 @@
 #include "src/Interval.h"
 #include "src/Solver.h"
 
+using namespace std;
 
 int main() {
     using boost::math::constants::pi;
     using boost::multiprecision::cpp_dec_float_50;
     int startTime = clock();
 
-    int intervalA = -12;
-    int intervalB = 20;
-    double (*derivative)(double)= &dfun5;
-    double (*function)(double)=&fun5;
-    double L = -1.02;
+    int intervalA = -13;
+    int intervalB = 13;
+    double (*derivative)(double)= &dfun1;
+    double (*function)(double)=&fun1;
+    double L = 1000;
     std::cout << "---------------------------" << std::endl;
     std::cout << "Data " << std::endl;
     std::cout << "Min x: " << intervalA << std::endl;
@@ -21,8 +22,8 @@ int main() {
     std::cout << "L: " << L << std::endl;
     std::cout << "---------------------------" << std::endl;
     Interval *nowy = new Interval(intervalA, intervalB, derivative, function, L);
-    Solver *solver = new Solver(*nowy, derivative, function);
-    solver->test();
+    Solver *solver = new Solver(*nowy, derivative, function,L);
+    solver->runAlgorithm();
 
     int duration = clock() - startTime;
     std::cout << "---------------------------" << std::endl;
--- src/Interval.h
+++ src/Interval.h
@@ -11,6 +11,7 @@
 #include <algorithm>
 
 class Interval {
+public:
     double a;
     double b;
     double funValA;
@@ -31,19 +32,27 @@ public:
         funValB = function(b);
     }
 
+    Interval(double a, double b) {
+        this->a = a;
+        this->b = b;
+    }
+
 public:
-    std::tuple<double,double> countIntervalCrossPoint() {
+    std::tuple<double,double,double> countIntervalCrossPoint() {
             isDericativeExist = true;
         double result;
             result = (funValA + L * a - funValB + L * b) / ( 2* L);
+            //policz jedna wybrana strone
+            double val = funValA-L*(result-a);
             if(result<a){
                 result = a+0.01;
             }
             else if(result>b){
                 result = b -0.01;
             }
-            //std::cout << "A: " << a << "   B:" << b << "   X: " << result << "     Y:" << function(result) << std::endl;
-            return std::make_tuple(result, function(result));
+            double resultVal = function(result);
+            std::cout << "A: " << a << "   B:" << b << "   X: " << result << "     Y:" << function(result) << std::endl;
+            return std::make_tuple(result, resultVal ,val);
 
     }
     double getH(double x){
@@ -52,11 +61,11 @@ public:
 
 public:
     double getSmallesValue() {
-        return (b > a) ? a : b;
+        return (b >= a) ? a : b;
     }
 
     double getBiggestValue() {
-        return (b > a) ? b : a;
+        return (b >= a) ? b : a;
     }
 
     double getFunValA() const {
